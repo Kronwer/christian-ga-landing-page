0ae2ed243985aff64106f3705f85c90d68a41e36
Christian Garcia Acidellas <christiangarcia11598@gmail.com>
2024-10-27 17:04:25 +0100

    Removed useless css property

--- components/Section/CV.vue
+++ components/Section/CV.vue
@@ -4,10 +4,10 @@
             <p class="font-semibold text-lg lg:text-xl">Download CV</p>
         </div>
         <div class="flex justify-between gap-3">
-            <a href="/(ENG)ChristianGarciaCV2024.pdf" download="(ENG)ChristianGarciaCV2024.pdf" class="flex grow">
+            <a href="/(ENG)ChristianGarciaCV2024.pdf" download="(ENG)ChristianGarciaCV2024.pdf" class="grow">
                 <UButton icon="i-uil-download-alt" color="cyan" variant="outline" size="xl" block>English</UButton>
             </a>
-            <a href="/(ESP)ChristianGarciaCV2024.pdf" download="(ESP)ChristianGarciaCV2024.pdf" class="flex grow">
+            <a href="/(ESP)ChristianGarciaCV2024.pdf" download="(ESP)ChristianGarciaCV2024.pdf" class="grow">
                 <UButton icon="i-uil-download-alt" color="red" variant="outline" size="xl" block>Spanish</UButton>
             </a>  
         </div>
