f5196c380ecab83c03a9b2ecc40594b1be2ac608
Christian Garcia Acidellas <christiangarcia11598@gmail.com>
2024-10-27 17:15:09 +0100

    Add nuxt hub to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxthub/core"
+  ],
   compatibilityDate: "2024-09-28"
 })
