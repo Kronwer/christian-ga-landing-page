0f87efda1ebe926f1e7185731d77ad918e428771
Christian Garcia Acidellas <christiangarcia11598@gmail.com>
2025-01-16 18:28:54 +0100

    Mark both CV outdated

--- components/Section/CV.vue
+++ components/Section/CV.vue
@@ -5,10 +5,10 @@
         </div>
         <div class="flex justify-between gap-3">
             <a href="/(ENG)ChristianGarciaCV2024.pdf" download="(ENG)ChristianGarciaCV2024.pdf" class="grow">
-                <UButton icon="i-uil-download-alt" color="cyan" variant="outline" size="xl" block>English</UButton>
+                <UButton icon="i-uil-download-alt" color="cyan" variant="outline" size="xl" block>English (Outdated)</UButton>
             </a>
             <a href="/(ESP)ChristianGarciaCV2024.pdf" download="(ESP)ChristianGarciaCV2024.pdf" class="grow">
-                <UButton icon="i-uil-download-alt" color="red" variant="outline" size="xl" block>Spanish</UButton>
+                <UButton icon="i-uil-download-alt" color="red" variant="outline" size="xl" block>Spanish (Outdated)</UButton>
             </a>  
         </div>
     </UCard>
