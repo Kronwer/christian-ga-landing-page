d6a8c07d78b3f9ee1ff080c2118274338249d231
Christian Garcia Acidellas <christiangarcia11598@gmail.com>
2025-01-29 22:35:31 +0100

    Actualizar CVs

--- components/Section/CV.vue
+++ components/Section/CV.vue
@@ -4,11 +4,11 @@
             <p class="font-semibold text-lg lg:text-xl xl:text-2xl">Download CV</p>
         </div>
         <div class="flex justify-between gap-3">
-            <a href="/(ENG)ChristianGarciaCV2024.pdf" download="(ENG)ChristianGarciaCV2024.pdf" class="grow">
-                <UButton icon="i-uil-download-alt" color="cyan" variant="outline" size="xl" block>English (Outdated)</UButton>
+            <a href="/(ENG)ChristianGarciaCV2025.pdf" download="(ENG)ChristianGarciaCV2025.pdf" class="grow">
+                <UButton icon="i-uil-download-alt" color="cyan" variant="outline" size="xl" block>English</UButton>
             </a>
-            <a href="/(ESP)ChristianGarciaCV2024.pdf" download="(ESP)ChristianGarciaCV2024.pdf" class="grow">
-                <UButton icon="i-uil-download-alt" color="red" variant="outline" size="xl" block>Spanish (Outdated)</UButton>
+            <a href="/(ESP)ChristianGarciaCV2025.pdf" download="(ESP)ChristianGarciaCV2025.pdf" class="grow">
+                <UButton icon="i-uil-download-alt" color="red" variant="outline" size="xl" block>Spanish</UButton>
             </a>  
         </div>
     </UCard>
